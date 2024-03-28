fab0a3c73ca3fd6ced66a88e72440f646a71d2fc
Luis Josue Saigua Janko <elasesino6633@gmail.com>
2024-03-28 17:07:41 -0400

    Disparo implementado

--- Source/Galaga_USFX_LAB01/EnemyShip.h
+++ Source/Galaga_USFX_LAB01/EnemyShip.h
@@ -18,18 +18,28 @@ public:
 	UPROPERTY(EditAnywhere)
 	UStaticMeshComponent* EnemyMesh;	
 
-	//Propiedad para el componente de movimiento
+	// Propiedad para el componente de movimiento
 	UPROPERTY(VisibleAnywhere, Category = "Movement")
 	UMovementPatternComponent* MovementComponent;
+
+	// Sonido de disparo
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	class USoundBase* FireSound;
+
+private:
+	// Declaracion de FireTimerHandle
+	FTimerHandle FireTimerHandle;
 
 public:	
 	// Sets default values for this actor's properties
 	AEnemyShip();
 
-	// Velocidad de la nave enemiga
-	// UPROPERTY(EditAnywhere, Category = "Movement")
-	float Speed = 150.0f;
-
+	// Atributos de la nave enemiga
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Speed;
+	bool ChangeMove;
+	float Count;
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -42,6 +52,8 @@ public:
 	// Funcion para mover la nave enemiga
 	void MoveEnemy(float DeltaTime);
 
-	// Funcion de movimiento con el componente de movimiento
 	void MoveEnemyPattern(float DeltaTime);
-};
+
+	// Funcion de disparo
+	void FireShot();
+}; 
